0dd7925e586ffe4c6aea94538d76fef738755aec
Kris Kata <kata.krzysztof@gmail.com>
2015-07-22 20:37:28 -0700

    implemented handler for touch cancelled

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -81,6 +81,19 @@
     [self setNeedsDisplay];
 }
 
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+    
+    [self setNeedsDisplay];
+}
+
 - (void)strokeLine:(BNRLine *)line
 {
     UIBezierPath *bp = [UIBezierPath bezierPath];
