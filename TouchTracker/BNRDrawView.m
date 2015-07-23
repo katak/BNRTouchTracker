c01a275c4c70ee911ddf6904f6f0382e4bcbc407
Kris Kata <kata.krzysztof@gmail.com>
2015-07-22 20:13:33 -0700

    added touch handlers to draw lines

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -7,6 +7,14 @@
 //
 
 #import "BNRDrawView.h"
+#import "BNRLine.h"
+
+@interface BNRDrawView ()
+
+@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
 
 @implementation BNRDrawView
 
@@ -14,18 +22,72 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
     }
+    
     return self;
 }
 
-/*
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *t = [touches anyObject];
+    
+    // Get location of the touch in view's coordinate system
+    CGPoint location = [t locationInView:self];
+    
+    self.currentLine = [[BNRLine alloc] init];
+    self.currentLine.begin = location;
+    self.currentLine.end = location;
+    
+    [self setNeedsDisplay];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *t = [touches anyObject];
+    CGPoint location = [t locationInView:self];
+    
+    self.currentLine.end = location;
+
+    [self setNeedsDisplay];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.finishedLines addObject:self.currentLine];
+    
+    self.currentLine = nil;
+    
+    [self setNeedsDisplay];
+}
+
+- (void)strokeLine:(BNRLine *)line
+{
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    // Draw finished lines in black
+    [[UIColor blackColor] set];
+    for (BNRLine *line in self.finishedLines) {
+        [self strokeLine:line];
+    }
+    
+    if (self.currentLine) {
+        // If there is a line currently being drawn, do it in red
+        [[UIColor redColor] set];
+        [self strokeLine:self.currentLine];
+    }
 }
-*/
 
 @end
